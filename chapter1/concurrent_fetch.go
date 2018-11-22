4333e86df3767ec01fceffd0e5bb531d9746c400
mevlanaayas <mevlanaayas@gmail.com>
2018-11-22 15:21:32 +0300

    implemented 250+ websites to use with concurrent get requests

--- chapter1/concurrent_fetch.go
+++ chapter1/concurrent_fetch.go
@@ -11,42 +11,58 @@ import (
 )
 
 type FetchResult struct {
-	content    string
+	length     float64
 	finishedAt int
 	url        string
+	order      int
 }
 
 func main() {
 	fmt.Println("concurrent fetch program to fetch all given urls")
 	const prefix = "http://"
+	urls := os.Args[1:]
+	data, err := ioutil.ReadFile("urls.txt")
+	if err == nil {
+		fmt.Println("reading urls from file...")
+		urls = strings.Split(string(data), "\n")
+	}
+	fmt.Println("size of urls: ", len(urls))
 	resultChannel := make(chan FetchResult)
 	programStart := time.Now()
-	for _, url := range os.Args[1:] {
+	for index, url := range urls {
 		if !strings.HasPrefix(url, prefix) {
 			url = prefix + url
 		}
-		go func(url string) {
+		go func(url string, index int) {
+			fmt.Printf("----- %d. http get started for: %s ------\n", index, url)
 			httpStart := time.Now()
+
 			response, err := http.Get(url)
 			if err != nil {
 				fmt.Println("GET: error time: %T, error msg: %T", time.Now(), err)
+				errorResult :=
+					FetchResult{0.0, time.Now().Second(), url, index}
+					resultChannel <- errorResult
+				return
 			}
 			content, err := ioutil.ReadAll(response.Body)
 			if err != nil {
 				fmt.Println("READ: error time: %T, error msg: %T", time.Now(), err)
 			}
-			fmt.Printf("\n url: %s, length of content: %d \n", url, len(content))
+			fmt.Printf("\n finished url: %s, length of content: %d", url, len(content))
 			httpElapsed := time.Since(httpStart).Seconds()
-			fmt.Println("url and elapsed: ", url, httpElapsed)
-			localResult := FetchResult{string(content)[:10], time.Now().Second(), url}
+			fmt.Printf("\n finished for url: %s  and elapsed time is: %f \n", url, httpElapsed)
+			localResult :=
+				FetchResult{float64(len(content) / 1024.0), time.Now().Second(), url, index}
 			resultChannel <- localResult
 			defer response.Body.Close()
-
-		}(url)
+		}(url, index)
 	}
-	for range os.Args[1:] {
+	for range urls {
 		result := <-resultChannel
-		fmt.Println(result)
+		fmt.Printf(
+			"size: %f, url: %s, finished At: %d, order: %d \n",
+			result.length, result.url, result.finishedAt, result.order)
 	}
 	programElapsed := time.Since(programStart).Seconds()
 	fmt.Println("elapsed time: ", programElapsed)
