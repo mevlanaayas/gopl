d31c6cb891cf355041e08aae16d9c1b80bc47ec4
mevlanaayas <mevlanaayas@gmail.com>
2018-11-22 12:58:39 +0300

    implemented benchmark

--- chapter1/concurrent_fetch.go
+++ chapter1/concurrent_fetch.go
@@ -20,11 +20,13 @@ func main() {
 	fmt.Println("concurrent fetch program to fetch all given urls")
 	const prefix = "http://"
 	resultChannel := make(chan FetchResult)
+	programStart := time.Now()
 	for _, url := range os.Args[1:] {
 		if !strings.HasPrefix(url, prefix) {
 			url = prefix + url
 		}
 		go func(url string) {
+			httpStart := time.Now()
 			response, err := http.Get(url)
 			if err != nil {
 				fmt.Println("GET: error time: %T, error msg: %T", time.Now(), err)
@@ -34,19 +36,18 @@ func main() {
 				fmt.Println("READ: error time: %T, error msg: %T", time.Now(), err)
 			}
 			fmt.Printf("\n url: %s, length of content: %d \n", url, len(content))
+			httpElapsed := time.Since(httpStart).Seconds()
+			fmt.Println("url and elapsed: ", url, httpElapsed)
 			localResult := FetchResult{string(content)[:10], time.Now().Second(), url}
 			resultChannel <- localResult
 			defer response.Body.Close()
 
 		}(url)
 	}
-	var input string
-	fmt.Print("Enter an input: ")
-	fmt.Scanln(&input)
-
 	for range os.Args[1:] {
 		result := <-resultChannel
 		fmt.Println(result)
 	}
-
+	programElapsed := time.Since(programStart).Seconds()
+	fmt.Println("elapsed time: ", programElapsed)
 }
