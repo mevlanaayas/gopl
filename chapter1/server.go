6fb0ae3326d89fa12d04fe083a4cc3880841c50f
mevlanaayas <mevlanaayas@gmail.com>
2018-11-22 16:59:22 +0300

    implemented request counter

--- chapter1/server.go
+++ chapter1/server.go
@@ -5,8 +5,11 @@ import (
 	"net/http"
 )
 
-func main()  {
+var count int
+
+func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
 	err := http.ListenAndServe(":9999", nil)
 	if err != nil {
 		panic(err)
@@ -16,5 +19,19 @@ func main()  {
 func handler(w http.ResponseWriter, r *http.Request) {
 	// we can also use Fprintf here instead of  w.Writer
 	// w.Write([]byte(r.URL.Path))
+	fmt.Println("--inside handler function--", r.URL.Path)
+	/*
+		when request comes to server it always call handler!!
+		for /favicon.ico
+		so we ignore other urls to guarantee exact count
+	*/
+	if r.URL.Path == "/favicon.ico" {
+		count++
+	}
 	fmt.Fprintf(w, "Requested url is: %s", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("--inside counter function--", r.URL.Path)
+	fmt.Fprintf(w, "Current count is so far: %d", count)
 }
