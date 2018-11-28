0c2f28881db29287c29924e8ad2dda31fd0e77cc
mevlanaayas <mevlanaayas@gmail.com>
2018-11-28 17:09:07 +0300

    created template for exercise 2.2

--- chapter2/exercisetwo/convert_machine.go
+++ chapter2/exercisetwo/convert_machine.go
@@ -1,4 +1,10 @@
-package exercisetwo
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 /*
 we will take any amount of numbers from command line.
@@ -12,6 +18,86 @@ FtoM MtoF // feet to meter - meter to feet
 PtoK KtoP // kilogram to pound- pound to kilogram
 */
 
-func main()  {
-	
+/*
+a good new: we will use interfaces (not the best case to use it but good to use it to practice)
+instead of using six different types like Celsius, Fahrenheit, Meter...
+we use a one CustomType
+and interface to accept all the functions of custom type.
+*/
+
+type CustomType float64
+
+func (f CustomType) CtoF() (c CustomType) {
+	/*
+		it is intentionally left default 5
+	*/
+	c = CustomType(5)
+	return
+}
+
+func (f CustomType) FtoC() (c CustomType) {
+	c = CustomType(5)
+	return
+}
+
+func (f CustomType) FtoM() (c CustomType) {
+	c = CustomType(5)
+	return
+}
+
+func (f CustomType) MtoF() (c CustomType) {
+	c = CustomType(5)
+	return
+}
+
+func (f CustomType) KtoP() (c CustomType) {
+	c = CustomType(5)
+	return
+}
+
+func (f CustomType) PtoK() (c CustomType) {
+	c = CustomType(5)
+	return
+}
+
+// ------------------------
+type Type interface {
+	CtoF() CustomType
+	FtoC() CustomType
+	FtoM() CustomType
+	MtoF() CustomType
+	KtoP() CustomType
+	PtoK() CustomType
+}
+
+func main() {
+	/*
+		for taken values we will create six different variables:
+			Celsius(givenValue)
+			Fahrenheit(givenValue)
+			Feet(givenValue)
+			Meter(givenValue)
+			Kilogram(givenValue)
+			Pound(givenValue)
+		for new version we only need CustomType(givenValue)
+	*/
+
+	// TODO: we only support command line input. we need to implement standard input :)
+	for _, arg := range os.Args[1:] {
+		if givenValue, err := strconv.ParseFloat(arg, 64); err == nil {
+			result(CustomType(givenValue))
+		}
+	}
+}
+
+func result(convertTypes ...Type) {
+	for _, convertType := range convertTypes {
+		fmt.Println("fahrenheit", convertType.CtoF())
+		fmt.Println("celsius", convertType.FtoC())
+		fmt.Println("meter", convertType.FtoM())
+		fmt.Println("feet", convertType.MtoF())
+		fmt.Println("pound", convertType.KtoP())
+		fmt.Println("kilogram", convertType.PtoK())
+		fmt.Println("-----------------------------")
+	}
 }
